6caa2ef6578a9cfe039253fa7bfb3e31fc38b98c
Eldoprano <eldoprano@gmail.com>
2021-04-12 00:01:34 +0000

    Romanjinizator added in tools

--- Cogs/tools.py
+++ Cogs/tools.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import romkan
 
 # En esta clase definimos los comandos de este cog
 class Tools(commands.Cog, name="BotTools"):
@@ -24,6 +25,21 @@ class Tools(commands.Cog, name="BotTools"):
 
         await ctx.send("Reload complete ✅")#,delete_after=15)
 
+
+    @commands.command(name = "romanjinice",
+                    aliases=["roma","rmj"],
+                    usage="romanjinice <texto en Kana>",
+                    description = "Convierte Kana a Romanji")
+    async def reloadCogs(self, ctx:commands.Context):
+        # Transform to romanji
+        romanjinized = romkan.to_roma(ctx.message.content)
+        # Remove command from result (This removes the first word)
+        # TODO: If user sends the command and the text in different lines,
+        #       this code will remove everythong until it finds a space (deleting a lo of text)
+        #       Solution: Detect this and crop until a \n 
+        romanjinized = romanjinized[romanjinized.find(" ")+1:]
+        # Send result
+        await ctx.send(romanjinized)
 
 def setup(bot:commands.Bot):
     bot.add_cog(Tools(bot))
